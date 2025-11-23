c783817f42c6bf4bd87194aec1410385f6542732
Krunal Vijay Jayale <141128071+krunaljayale@users.noreply.github.com>
2025-11-23 18:58:33 +0530

    Disable COD availability check temporarily
    Comment out COD availability check logic in customer controller.

--- controllers/customer.js
+++ controllers/customer.js
@@ -368,16 +368,16 @@ module.exports.hotelData = async (req, res) => {
     hotelData.distance = rawDistance.toFixed(2);
     hotelData.estimatedTime = Math.max(10, Math.round(rawTime));
 
-    if (hotelData.isCODAvailable) {
-      const hasDeliveredPastOrder = await PastOrder.exists({
-        customer: _id,
-        status: "DELIVERED",
-      });
+   // if (hotelData.isCODAvailable) {
+      //const hasDeliveredPastOrder = await PastOrder.exists({
+       // customer: _id,
+       // status: "DELIVERED",
+     // });
 
-      if (!hasDeliveredPastOrder || isAfterCutoffIST()) {
-        hotelData.isCODAvailable = false;
-      }
-    }
+      //if (!hasDeliveredPastOrder || isAfterCutoffIST()) {
+        //hotelData.isCODAvailable = false;
+      //}
+    //}
 
     res.send(hotelData);
   } catch (error) {
